376c5ece9dfdf831af28fc2f3d8c5e621ceeb8d0
h3 <max@motion-m.ca>
2011-10-08 00:42:29 -0400

    Added term color support

--- dad/templates/dev.sh
+++ dad/templates/dev.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+
+# colors
+PS1="\e[0;36m(`basename \"$VIRTUAL_ENV\"`)\e[0m $_OLD_VIRTUAL_PS1"
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+#alias gst='git status '
+alias gc='git commit '
+alias gca='git commit -a '
+alias ga='git add '
+alias gco='git checkout '
+alias gb='git branch '
+alias gm='git merge '
+alias gt='git status'
+
 export WORKON_HOME=%(env_path)s
 %(activate_path)s
 cd %(project_name)s
