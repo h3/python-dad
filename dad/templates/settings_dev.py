d4de017cf8141d2dd95ace5eca02e445bb889a71
h3 <max@motion-m.ca>
2011-10-24 00:19:31 -0400

    Added debug toolbar to settings_dev

--- dad/templates/settings_dev.py
+++ dad/templates/settings_dev.py
@@ -6,6 +6,7 @@ sys.path.append("../")
 
 DEV = True
 DEBUG = True
+DEBUG_TOOLBAR = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
@@ -14,8 +15,21 @@ DATABASES = {
         'NAME': os.path.join(os.path.dirname(
                 os.path.abspath(__file__)), 'dev.db'),
     },
+#   'default': {
+#       'ENGINE': 'django.db.backends.mysql',
+#       'NAME': '',
+#       'USER': '',
+#       'PASSWORD': '',
+#       'HOST': '',
+#       'PORT': '',
+#   },
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-TEMPLATE_STRING_IF_INVALID = '{{ Variable not found }}'
+#TEMPLATE_STRING_IF_INVALID = '{{ Variable not found }}'
+
+if DEBUG_TOOLBAR:
+    INTERNAL_IPS = ('127.0.0.1', '192.168.1.103')
+    INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+    MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
